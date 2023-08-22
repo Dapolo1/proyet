647693ba460fdb7b935daad73d60d17fadb4950f
Daniel Polo <danielandrespolooviedo08@gmail.com>
2023-08-22 17:40:16 -0500

    hola

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Agregar nombre del jugador
-nombres = input("escriba su nombre: ")
-apellidos = input(f"escriba sus apellidos, {nombres}: ")
+nombre = input("escriba su nombre: ")
+
 print(
-    f"Bienvenido estimado ingeniero, {nombres} {apellidos}, hoy es un gran dia, gracias.")
+    f"Bienvenido estimado ingeniero, {nombre}, hoy es un gran dia, gracias.")
